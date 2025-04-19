8a2019f06c82e2c90e93d22fcf4bb9c0e123a226
kajukatli457 <kajukatli457457@gmail.com>
2025-04-19 09:35:47 +0530

    Image slideshow added to project section.

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	AOS.init();
 });
+
+// Select all carousels on the page
+document.querySelectorAll('.carousel').forEach(function (carousel) {
+    const images = carousel.querySelectorAll('.carousel-img');
+    const prevBtn = carousel.querySelector('.prev');
+    const nextBtn = carousel.querySelector('.next');
+    let currentIndex = 0;
+
+    // Show image at given index
+    function showImage(index) {
+      images.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
+      });
+    }
+
+    // Previous button
+    prevBtn.addEventListener('click', function () {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      showImage(currentIndex);
+    });
+
+    // Next button
+    nextBtn.addEventListener('click', function () {
+      currentIndex = (currentIndex + 1) % images.length;
+      showImage(currentIndex);
+    });
+
+    // Initialize first image
+    showImage(currentIndex);
+  });
